b178037e3c0748dcfcdb99e0c559204de7ac79f6
Clemens Park <clemens.park@gmail.com>
2013-03-15 11:31:58 -0400

    adds party creation

--- client/client.js
+++ client/client.js
@@ -1,5 +1,12 @@
-Template.partyList.events({
+Template.partyCreation.events({
   "click #party-creation .create": function(event, template) {
+    var name = template.find(".party-name").value
+    Parties.insert({name: name})
+
     return false;
   }
 })
+
+Template.partyList.parties = function() {
+  return Parties.find({})
+}
