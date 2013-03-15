2c46069208c92fa9f2a464d7914aa26750e2a6ca
Clemens Park <clemens.park@gmail.com>
2013-03-15 13:35:45 -0400

    adds the party details section

--- client/client.js
+++ client/client.js
@@ -10,3 +10,16 @@ Template.partyCreation.events({
 Template.partyList.parties = function() {
   return Parties.find({})
 }
+
+Template.partyList.events({
+  "click .party-header": function() {
+    Session.set("showPartyDetailsFor", this.name)
+
+    return false;
+  }
+})
+
+Template.partyList.showDetailsFor = function(partyName) {
+  var showPartyDetailsFor = Session.get("showPartyDetailsFor")
+  return showPartyDetailsFor === partyName
+}
--- rsvp.js
+++ rsvp.js
@@ -1,1 +1,35 @@
 Parties = new Meteor.Collection("parties")
+
+
+
+
+
+
+// Invitations = new Meteor.Collection("invitations")
+// 
+// Meteor.methods({
+//   createInvitationForParty: function(party, inviteeName) {
+//     if (!party || !inviteeName) {
+//       return new Meteor.Error(500, "Missing fields")
+//     }
+//     var inviteCode = generateUUID()
+// 
+//     Invitations.insert({
+//       inviteCode: inviteCode,
+//       numberOfGuests: 0,
+//       party: party,
+//       inviteeName: inviteeName
+//     })
+//   },
+// 
+//   generateUUID: function() {
+//     var s4 = function() {
+//       return Math.floor((1 + Math.random()) * 0x10000)
+//       .toString(16)
+//       .substring(1)
+//     }
+// 
+//     return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+//       s4() + '-' + s4() + s4() + s4()
+//   }
+// })
